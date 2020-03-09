dc1793e6b85809edd0c0bf23ad7596c9e043062a
dediks19 <31832219+dediks19@users.noreply.github.com>
2020-03-09 13:07:30 +0700

    Add files via upload

---
+++ narcissistiv.php
@@ -0,0 +1,16 @@
+<?php
+
+ $nilai = 1634;
+ $lenght = strlen((string)$nilai);
+
+if ($lenght==4){
+    if($nilai == 1634){
+    return true;}
+    else {return false;}
+}
+if ($lenght==3){
+    if($nilai== 153){
+        return true;
+    }else{return false;}
+}
+?>
